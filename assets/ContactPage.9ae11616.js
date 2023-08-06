4ebe39dd48492b289af3b18766e52f97fe8bcc45
Marimoiro <5342721+Marimoiro@users.noreply.github.com>
2023-08-06 06:12:58 +0000

    Deploying to gh-pages from @ Marimoiro/trpg-data-template@e63016b5fff088170a618dda197cedb9bfac8745 🚀

---
+++ assets/ContactPage.9ae11616.js
@@ -0,0 +1,38 @@
+import{Q as N,_ as s,a,b as c,T as A}from"./TTitle.9d56cab7.js";import{_ as T}from"./EssentialLink.6a3e78c6.js";import{F as u,G as e,I as o,J as n,K as i,R as I,S as O,V as r,T as E,Q as S}from"./index.2ef42853.js";import"./render.22b1b71b.js";import"./use-dark.d708c546.js";import"./use-router-link.4ec760f9.js";import"./use-size.fb70cad8.js";var h=[{title:"\u30DA\u30FC\u30B8\u7BA1\u7406\u30EA\u30DD\u30B8\u30C8\u30EA",url:"https://github.com/Marimoiro/dx3rd-syndrome-oracle",license:`MIT License\r
+\r
+Copyright (c) 2023 Marimoiro\r
+\r
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\r
+\r
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\r
+\r
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`},{title:"Quasar",url:"https://github.com/quasar/Quasar",license:`MIT License\r
+\r
+Copyright (c) 2023 MaxXor\r
+\r
+Permission is hereby granted, free of charge, to any person obtaining a copy\r
+of this software and associated documentation files (the "Software"), to deal\r
+in the Software without restriction, including without limitation the rights\r
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\r
+copies of the Software, and to permit persons to whom the Software is\r
+furnished to do so, subject to the following conditions:\r
+\r
+The above copyright notice and this permission notice shall be included in all\r
+copies or substantial portions of the Software.\r
+\r
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\r
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\r
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\r
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\r
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\r
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\r
+SOFTWARE.`},{title:`quasar-admin\r
+`,url:"https://github.com/pratik227/quasar-admin",license:`MIT License\r
+\r
+Copyright (c) 2022 Pratik Patel\r
+\r
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\r
+\r
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\r
+\r
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`}],d=[{title:"Twitter(\u30D0\u30B0\u3084\u610F\u898B)",name:"@Marimoiro",value:"https://twitter.com/Marimoiro"},{title:"GitHub",name:"@Marimoiro",value:"https://github.com/Marimoiro"}];const m=["innerHTML"],D=u({__name:"ContactPage",setup(p){const R=d,l=h;return(f,H)=>(e(),o(N,{class:"q-pa-lg"},{default:n(()=>[i(s,{title:"\u9023\u7D61\u5148"}),i(a,{title:"\u9023\u7D61\u5148"},{default:n(()=>[(e(!0),I(E,null,O(r(R),t=>(e(),o(T,{title:t.title+":"+t.name,key:t.title,link:t.value},null,8,["title","link"]))),128))]),_:1}),i(c),i(s,{title:"\u30E9\u30A4\u30BB\u30F3\u30B9\u8868\u8A18"}),(e(!0),I(E,null,O(r(l),t=>(e(),o(a,{title:t.title,key:t.title},{default:n(()=>[i(T,{title:t.title,link:t.url},null,8,["title","link"]),S("div",{innerHTML:`<p>${r(A)(t.license)}</p>`},null,8,m)]),_:2},1032,["title"]))),128))]),_:1}))}});export{D as default};
